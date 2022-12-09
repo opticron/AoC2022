0ff4bd9f127fc06b86a49f16e6ad1fce6d8fe728
Kinsey Moore <nyphbl8d@gmail.com>
2022-12-08 21:09:23 -0600

    day4: part 2 done

--- day4.rs
+++ day4.rs
@@ -5,16 +5,28 @@ fn get_range(range: &str) -> Vec<u32> {
   return range.split("-").map(|rpoint| rpoint.parse::<u32>().expect("Bad conversion")).collect::<Vec<u32>>();
 }
 
+fn target_in_range(target: u32, range: &Vec<u32>) -> bool {
+  if target >= range[0] && target <= range[1] {
+    return true;
+  }
+  return false;
+}
+
 fn get_contained(entrystring: &str) -> u32 {
   let entries: Vec<&str> = entrystring.split(",").collect();
   let entry1r: Vec<u32> = get_range(entries[0]);
   let entry2r: Vec<u32> = get_range(entries[1]);
-  // first contained in second
-  if entry1r[0] >= entry2r[0] && entry1r[1] <= entry2r[1] {
+
+  if target_in_range(entry2r[0], &entry1r) {
     return 1;
   }
-  // second contained in first
-  if entry2r[0] >= entry1r[0] && entry2r[1] <= entry1r[1] {
+  if target_in_range(entry2r[1], &entry1r) {
+    return 1;
+  }
+  if target_in_range(entry1r[0], &entry2r) {
+    return 1;
+  }
+  if target_in_range(entry1r[1], &entry2r) {
     return 1;
   }
   return 0;
